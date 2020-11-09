354da9eaa8237b7785ada4002a14ecbdb5d79b06
python-WTB <74200974+python-WTB@users.noreply.github.com>
2020-11-09 13:11:49 -0600

    Update calc-for-windows.py

--- calc-for-windows.py
+++ calc-for-windows.py
@@ -1,5 +1,5 @@
 #This is a Calculator for addition, subtraction, division, and multiplication.
-#Be sure add pull requests!!! 
+#Be sure to add pull requests!!! 
 import os
 clear = lambda:os.system('cls')
 clear()
